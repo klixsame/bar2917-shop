cbef177de1078aba4c7295d3b48bcd4147014dc5
klixsame <kleyfie92@gmail.com>
2025-12-15 02:32:19 +0300

    some change

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/faviconn.ico" />
+      <link rel="icon" href="/favicon.ico" />
       <title>Bar2917 - Суши-бар</title>
       <meta name="description" content="Лучший суши-бар - Bar2917. Свежие и вкусные роллы, суши, сеты. Быстрая доставка." />
     </>
